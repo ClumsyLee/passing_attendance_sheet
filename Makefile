1aa217c19c7a4d4fb8ecc62068241638bb61b371
ThomasLee911 <lisihan969@gmail.com>
2014-04-13 16:53:20 +0800

    add main.cpp and Makefile

---
+++ Makefile
@@ -0,0 +1,16 @@
+CFLAG = -c -std=c++11 -O2 -Wall -Wextra
+
+bin/pass_sheet: obj/main.o obj/classroom.o | bin
+    g++ -o $@ $^
+
+obj/main.o: src/main.cpp src/classroom.h | obj
+    g++ $(CFLAG) -o $@ $<
+
+obj/classroom.o: src/classroom.cpp src/classroom.h | obj
+    g++ $(CFLAG) -o $@ $<
+
+bin: mkdir bin
+obj: mkdir obj
+
+clean:
+    -rm obj/*.o
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include "classroom.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc == 6)
+    {
+        int row = std::atoi(argv[1]);
+        int col = std::atoi(argv[2]);
+        int start_row = std::atoi(argv[3]);
+        int start_col = std::atoi(argv[4]);
+        int turns = std::atoi(argv[5]);
+
+        passing_attendance_sheet::Classroom classroom(row, col);
+        for (int i = 0; i < turns; i++)
+        {
+            std::cout << "turn " << i << ": "
+                      << classroom.PassSheet(start_row, start_col)
+                      << std::endl;
+        }
+    }
+
+    return 0;
+}
